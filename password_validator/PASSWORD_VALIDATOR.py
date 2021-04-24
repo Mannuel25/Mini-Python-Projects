1d9bd1e34c2accb47d0b0ca8e12c4276283ac334
Mannuel25 <oluwasegunprosperity@gmail.com>
2021-04-24 21:17:58 +0100

    addeed PASSWORD_VALIDATOR.py

--- password_validator/PASSWORD_VALIDATOR.py
+++ password_validator/PASSWORD_VALIDATOR.py
@@ -1,1 +1,38 @@
+import string
 
+def passwordValidator():
+    """
+    Valids passwords to match specific rules
+    : return: str
+    """
+    print('\nYour password should: ')
+    print('\t- Have a minimum length of 6;')
+    print('\t- Have a maximum length of 12;')
+    print('\t- Contain at least an uppercase letter or a lowercase letter')
+    print('\t- Contain at least a number;')
+    print('\t- Contain at least a special character (such as @,+,£,$,%,*^,etc);')
+    print('\t- Not contain space(s).')
+
+    userPassword = input('\nEnter a valid password: ').strip()
+    if not(6 <= len(userPassword) <= 12):
+        message = 'Invalid Password..your password should have a minimum '
+        message += 'length of 6 and a maximum length of 12'
+        return message
+    if ' ' in userPassword:
+       message = 'Invalid Password..your password shouldn\'t contain space(s)'
+       return message
+    if not any(i in string.ascii_letters for i in userPassword):
+       message = 'Invalid Password..your password should contain at least '
+       message += 'an uppercase letter and a lowercase letter'
+       return message
+    if not any(i in string.digits for i in userPassword):
+        message = 'Invalid Password..your password should contain at least a number'
+        return message
+    if not any(i in string.punctuation for i in userPassword): 
+       message = 'Invalid Password..your password should contain at least a special character'
+       return message
+    else:
+       return 'Valid Password!'
+
+my_password = passwordValidator()
+print(my_password)
