8d09a475eebf5d11f615c1111f0f4002898e5619
Mannuel25 <oluwasegunprosperity@gmail.com>
2021-04-24 21:09:49 +0100

    updated emailSlicer.py

--- email_slicer/emailSlicer.py
+++ email_slicer/emailSlicer.py
@@ -3,7 +3,7 @@ def EmailSlicer():
     Displays the username and domain name of an email address
     : return: None
     """
-    emailAddress = input('Enter your email address: ')
+    emailAddress = input('Enter your email address: ').strip()
     split_address = emailAddress.split('@')
     print(f'Your username is {split_address[0]}, while the domain name is {split_address[1]}')
 
