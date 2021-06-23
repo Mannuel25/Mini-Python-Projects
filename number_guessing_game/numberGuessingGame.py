57775f2cb262bb19c83d97c56856066850da5ef2
Tanimowo Emmanuel <oluwasegunprosperity@gmail.com>
2021-06-23 20:06:09 +0100

    Updated numberGuessingGame.py

--- number_guessing_game/numberGuessingGame.py
+++ number_guessing_game/numberGuessingGame.py
@@ -1,4 +1,5 @@
 import random
+# random module is a built-in module to generate the pseudo-random variables
 
 def display_gameplay():
     """
@@ -6,16 +7,20 @@ def display_gameplay():
     : return: None
     """
     print('\nWelcome to Number Guessing Game!')
-    print('In this game you\'ve just 10 trials to guess a number between the range of 1-10')
+    print('In this game you\'ve just 7 trials to guess a number between the range of 1-10')
     print('Note: enter \'exit\' to end game')
 
 def startGame():
     """
-    Gets user response to start the game
+    Gets user response to start or end the game
     : return: str
     """
+    # call the function to display gameplay
     displayGameplay = display_gameplay()
+    # make a list of the possible inputs
+    # to start or end the game
     POSSIBLE_RESPONSES = ['Y','YES','N','NO','EXIT']
+    # get user's response
     user_response = input('\nStart game? (yes/no): ').strip().upper()
     while user_response not in POSSIBLE_RESPONSES:
         print('\nInvalid Input!')
@@ -27,42 +32,51 @@ def game():
     Controls the game
     : return: None
     """
+    # call the function to get user's response
     play_game = startGame()
+    # assign the number of trials the user has to a variable
     number_of_trials = 7
     while play_game == 'YES' or play_game == 'Y':
-        POSSIBLE_NUMBER_PICKS = [str(i) for i in range(1,11)]
-        user_number = input('\nGuess a number between the range of 1-10: ').strip().upper()
-        while user_number not in POSSIBLE_NUMBER_PICKS and user_number != 'EXIT' :
+        # make a list that contains the
+        # accepted number a user can guess
+        ACCEPTED_NUMBER_PICKS = [str(i) for i in range(1,11)]
+        # get user's number
+        user_input = input('\nGuess a number between the range of 1-10: ').strip().upper()
+        while user_input not in ACCEPTED_NUMBER_PICKS and user_input != 'EXIT' :
             print('Invalid Input!')
-            user_number = input('\nGuess a valid number between the range of 1-10: ').strip().upper()
-        if user_number == 'EXIT':
+            user_input = input('\nGuess a valid number between the range of 1-10: ').strip().upper()
+        if user_input == 'EXIT':
             print('Bye Player!')
             break
         else:
+            # generate a random number in the range 1-10
+            # and assign it to a variable
             computer_number = random.randint(1,10)
-            user_number = int(user_number)
-            if user_number < computer_number:
+            user_input = int(user_input)
+            if user_input < computer_number:
                 number_of_trials -= 1
-                print(f'Oops, {user_number} is too low')
-                if number_of_trials != 0:
-                    print(f'You\'ve {number_of_trials} trial(s) left')
-                    play_game = input('\nGuess again? (yes/no): ').strip().upper()
-                else:
-                    print(F'\nGame over!, you\'ve 0 trial left')
-                    break
-            elif user_number > computer_number:
-                number_of_trials -= 1
-                print(f'Oops, {user_number} is too high')
+                print(f'Oops, {user_input} is too low')
                 if number_of_trials != 0:
                     print(f'You\'ve {number_of_trials} trial(s) left')
                     play_game = input('\nGuess again? (yes/no): ').strip().upper()
                 else:
                     print(F'\nGame over!, you\'ve 0 trial left..try harder next time 😉')
                     break
-            elif user_number == computer_number:
+            elif user_input > computer_number:
+                number_of_trials -= 1
+                print(f'Oops, {user_input} is too high')
+                if number_of_trials != 0:
+                    print(f'You\'ve {number_of_trials} trial(s) left')
+                    play_game = input('\nGuess again? (yes/no): ').strip().upper()
+                else:
+                    print(F'\nGame over!, you\'ve 0 trial left..try harder next time 😉')
+                    break
+            elif user_input == computer_number:
                 number_of_trials -= 1
                 print(f'Congratulations!!..you guessed right, after {7 - number_of_trials} trial(s)')
                 play_game = input('\nDo you wish to play again? (yes/no): ').strip().upper()
+                # if the user wishes to play again, assign
+                # the number of trials the user has to a variable
                 number_of_trials = 7 
 
 game()
