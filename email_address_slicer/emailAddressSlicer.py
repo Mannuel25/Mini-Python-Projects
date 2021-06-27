3eaa58edf17525f37ad275d2bef376e46afd0a79
Tanimowo Emmanuel <oluwasegunprosperity@gmail.com>
2021-06-27 23:13:15 +0100

    restructured code

--- email_address_slicer/emailAddressSlicer.py
+++ email_address_slicer/emailAddressSlicer.py
@@ -3,8 +3,17 @@ def EMAIL_ADDRESS_SLICER():
     Displays the username and domain name of an email address
     : return: None
     """
+    # get user's email address
     emailAddress = input('Enter your email address: ').strip()
-    split_address = emailAddress.split('@')
-    print(f'Your username is {split_address[0]}, while the domain name is {split_address[1]}')
+    try:
+        # make a list with the email address
+        split_address = emailAddress.split('@')
+        # get both the username and domain name
+        # using list indexing
+        user_name, domain_name = split_address[0], split_address[1]
+    except IndexError:
+        print('Invalid email address!')
+    else:
+        print(f'Your username is {user_name}, while the domain name is {domain_name}')
 
 EMAIL_ADDRESS_SLICER()
